6d697371d0cfb73c0c37aabf8691baef7b12155a
mrockway <mrockway@gmail.com>
2015-11-09 01:21:16 -0800

    added winner determination, css styling, etc

--- main.js
+++ main.js
@@ -3,150 +3,173 @@
 // JS has loaded properly
 console.log('Play Ball!');
 
-var teamData = [];
+var awayWin = 0;
+var homeWin = 0;
+var p1Pos = 0;
+var p2Pos = 0;
+var homeTeam = "";
+var awayTeam = "";
 
-//Massive object of team names/colors
-function Team (name,color1,color2) {
-	this.name = name;
-	this.color1 = color1;
-	this.color2 = color2;
-	teamData.push({team : [this.name, this.color1, this.color2]});
-}
+$('.playNext').hide();
+$('.ball').hide();
+$('.scoreBoard').hide();
+$('.gameBoard').hide();
+$('#teamName2').hide();
+$('.seriesWinner').hide();
+
+
+
 //Select team name on change
-
 function displayTeam1() {
     var tm1 = document.getElementById("teamName1");
     var i = tm1.selectedIndex;
-    $('#away').replaceWith('<div id="away"><h2>'+tm1.options[i].text+'</h2><br><img class="logo" src=./team_logos/'+tm1.options[i].id+'.png></div>');
+    awayTeam = tm1.options[i].text;
+    $('#away').replaceWith('<div id="away"><img class="logo" src=./team_logos/'+tm1.options[i].id+'.png><h2 class="teamName">'+tm1.options[i].text+'</h2></div>');
+    $('#teamName1').hide();
+    $('#teamName2').show();
+    return awayTeam;
 }
 
 function displayTeam2() {
     var tm2 = document.getElementById("teamName2");
     var i = tm2.selectedIndex;
-    $('#home').replaceWith('<div id="away"><h2>'+tm2.options[i].text+'</h2><br><img class="logo" src=./team_logos/'+tm2.options[i].id+'.png></div>');
+    homeTeam = tm2.options[i].text;
+    console.log(homeTeam);
+    $('#home').replaceWith('<div id="home"><img class="logo" src=./team_logos/'+tm2.options[i].id+'.png><h2 class="teamName">'+tm2.options[i].text+'</h2></div>');
+    $('#teamName2').hide();
+    $('.gameBoard').show();
+    $('.scoreBoard').show();
+    $('.select').hide();
+    return homeTeam;
 }
-
-var diamondbacks = new Team ("Diamondbacks",'#A71930','#DBCEAC');
-var braves  = new Team ("Braves",'#002F5F','#B71234');
-var orioles  = new Team ("Orioles",'#ed4c09','#000000');
-var red_sox  = new Team ("Red Sox",'#C60C30','#002244');
-var cubs  = new Team ("Cubs",'#003279','#CC0033');
-var white_sox  = new Team ("White Sox",'#000000','#C0C0C0');
-var reds  = new Team ("Reds",'#C6011F','#000000');
-var indians  = new Team ("Indians",'#003366','#d30335');
-var rockies  = new Team ("Rockies",'#333366','#C0C0C0');
-var tigers  = new Team ("Tigers",'#001742','#DE4406');
-var astros  = new Team ("Astros",'#072854','#FF7F00');
-var royals  = new Team ("Royals",'#15317E','#74B4FA');
-var angels  = new Team ("Angels",'#B71234','#002244');
-var dodgers  = new Team ("Dodgers",'#083C6B');
-var marlins  = new Team ("Marlins",'#F9423A','#8A8D8F');
-var brewers  = new Team ("Brewers",'#182B49','#92754C');
-var twins  = new Team ("Twins",'#072754','#C6011F');
-var mets  = new Team ("Mets",'#002C77','#FB4F14');
-var yankees  = new Team ("Yankees",'#1C2841','#808080');
-var athletics  = new Team ("A's",'#003831','#FFD800');
-var phillies  = new Team ("Phillies",'#BA0C2F','#003087');
-var pirates  = new Team ("Pirates",'#000000','#fdb829');
-var padres  = new Team ("Padres",'#002147','#B4A76C');
-var giants  = new Team ("Giants",'#F2552C','#FFFDD0');
-var mariners  = new Team ("Mariners",'#0C2C56','#005C5C');
-var cardinals  = new Team ("Cardinals",'#c41e3a','#0A2252');
-var rays  = new Team ("Rays",'#00285D','#9ECEEE');
-var rangers  = new Team ("Rangers",'#BD1021','#003279');
-var blue_jays  = new Team ("Blue Jays",'#003DA5','#041E42');
-var nationals  = new Team ("Nationals",'#BA122B','#11225B');
-
-//Create scoreboard
-function scoreBoard (){
-
-	('.scoreBoard').append();
-}
+//Eventually use to color backgrounds with team colors
+var teamData = [["TeamName","Color1","Color2"],
+								["Diamondbacks",'#A71930','#DBCEAC'],
+								["Braves",'#002F5F','#B71234'],
+								["Orioles",'#ed4c09','#000000'],
+								["Red Sox",'#C60C30','#002244'],
+								["Cubs",'#003279','#CC0033'],
+								["White Sox",'#000000','#C0C0C0'],
+								["Reds",'#C6011F','#000000'],
+								["Indians",'#003366','#d30335'],
+								["Rockies",'#333366','#C0C0C0'],
+								["Tigers",'#001742','#DE4406'],
+								["Astros",'#072854','#FF7F00'],
+								["Royals",'#15317E','#74B4FA'],
+								["Angels",'#B71234','#002244'],
+								["Dodgers",'#083C6B'],
+								["Marlins",'#F9423A','#8A8D8F'],
+								["Brewers",'#182B49','#92754C'],
+								["Twins",'#072754','#C6011F'],
+								["Mets",'#002C77','#FB4F14'],
+								["Yankees",'#1C2841','#808080'],
+								["A's",'#003831','#FFD800'],
+								["Phillies",'#BA0C2F','#003087'],
+								["Pirates",'#000000','#fdb829'],
+								["Padres",'#002147','#B4A76C'],
+								["Giants",'#F2552C','#FFFDD0'],
+								["Mariners",'#0C2C56','#005C5C'],
+								["Cardinals",'#c41e3a','#0A2252'],
+								["Rays",'#00285D','#9ECEEE'],
+								["Rangers",'#BD1021','#003279'],
+								["Blue Jays",'#003DA5','#041E42'],
+								["Nationals",'#BA122B','#11225B']];
 
 //Create Countdown Timer
-$('.playBall').click(function(event){
-	var count = 3;
+ $('.playBall').click(function(event){
+	$('button').hide();
+	var count = 4;
 	function timer() {
-		if (count > 0) {
-			$('.timerClass').addClass('counter');
-			$('.timerClass').text(count);
+		if (count > 1) {
 			count --;
-			console.log(count);
+			$('.timerClass').text(count);
+			setTimeout(timer, 1000);
+		}
+		else if (count === 1) {
+			$('.timerClass').text('PLAY BALL');
+			count --;
 			setTimeout(timer, 1000);
 		}
 		else if (count === 0) {
-			$('.timerClass').text("Play Ball");
-			$('.timerClass').removeClass('counter');
-			setTimeout(timer,1000);
+			$('.ball').show();
 			$('.timerClass').hide();
 			gamePlay();
 		}
 	}
 	timer();
+ });
+
+// Set up restart button
+
+
+//Play the next game, keeps win count
+$('.playNext').click(function(event) {
+	$('.playBall').show();
+	$('.restart').show();
+	$('.timerClass').show();
+	$('.ball').hide();
+	$('.playNext').hide();
+	$('.displayWinner').hide();
+	$('.timerClass').text('');
+	$('.awayBall').css({left: 0});
+	$('.awayBall').removeClass('spin');
+	$('.homeBall').css({left: 0});
+	$('.homeBall').removeClass('spin');
+	p1Pos = 0;
+	p2Pos = 0;
+	return p1Pos,p2Pos;
 });
 
-// var count = 4;
-// function anim() {
-//     if (count > 0) {
-//         console.log(count);
-//         count--;
-//         setTimeout(anim, 700);
-//     }
-//     else {
-//         alert('hi');
-//     }
-// }
-// anim();
 
-// Set up reset button
-//Bring Players back to starting line
+//Complete restart of game
 $('.restart').click(function(event) {
-	$('.awayBall').css('left', '10px');
+	$('.playBall').show();
+	$('#teamName1').show();
+	$('#teamName2').show();
+	$('.title').show();
+	$('.timerClass').show();
+	$('.seriesWinner').hide();
+	$('.ball').hide();
+	$('.scoreBoard').hide();
+	$('button').hide();
+	$('.gameBoard').hide();
+	$('.timerClass').text('');
+	$('.awayBall').css({left: 0});
 	$('.awayBall').removeClass('spin');
-	$('.homeBall').css('left', '10px');
+	$('.homeBall').css({left: 0});
 	$('.homeBall').removeClass('spin');
 	p1Pos = 10;
 	p2Pos = 10;
-	console.log(p1Pos+"You clicked reset."+p2Pos);
-	return p1Pos, p2Pos;
+	homeWin = 0;
+	homeTeam = '';
+	awayTeam = '';
+	awayWin = 0;
+	return p1Pos,p2Pos,homeWin,awayWin;
 });
 
-var p1Pos = 10;
-var p2Pos = 10;
+
 //Set up Player 1 moving by pressing "A"
 function gamePlay () {
 $(window).keypress(function(event) {
-	console.log(p1Pos);
-	if (p1Pos <= 1100 && p2Pos <=1100) {
+	if (p1Pos <= 960 && p2Pos <=960) {
 		if (event.which === 97) {
 			$('.awayBall').addClass('spin');
-			$('.awayBall').stop().animate({left: "+=25"}, 'fast');
+			$('.awayBall').css({left: "+=25"}, 'fast');
 			p1Pos = p1Pos+25;
 			determineWinner(p1Pos,p2Pos);
-			console.log(p1Pos);
-			return p1Pos, p2Pos;
-		}
-		else {
-			console.log('player 2 already won.');
 		}
 	}
 });
 
 //Set up Player 2 moving by pressing "L"
 $(window).keypress(function(event) {
-	console.log(p2Pos);
-	if (p2Pos <= 1100 && p1Pos <=1100) {
+	if (p2Pos <= 960 && p1Pos <=960) {
 		if (event.which === 108) {
 			$('.homeBall').addClass('spin');
-			$('.homeBall').stop().animate({left: "+=25"}, 'fast');
+			$('.homeBall').css({left: "+=25"}, 'fast');
 			p2Pos = p2Pos+25;
-			console.log(p2Pos);
 			determineWinner(p1Pos,p2Pos);
-			return p1Pos, p2Pos;
 		}
-	}
-	else {
-		console.log('player 1 already won.');
 	}
 });
 }
@@ -154,18 +177,34 @@ $(window).keypress(function(event) {
 //Determine Winner
 
 function determineWinner (p1Pos, p2Pos) {
-	if (p1Pos >=1100 && p2Pos < p1Pos ) {
-		alert("p1 wins");
+	if (p1Pos >=960 && p2Pos < p1Pos ) {
+		$('.displayWinner').text(awayTeam + " Win");
 		awayWin++;
-		scoreBoard(awayWin);
-		return awayWin;
+		$('.awayRuns').text('Wins: '+awayWin);
+		$('.playNext').show();
 	}
-	else if (p2Pos >=1100 && p1Pos < p2Pos ){
-		alert("p2 wins");
+	else if (p2Pos >=960 && p1Pos < p2Pos ){
+		$('.displayWinner').text(homeTeam + " Win");
 		homeWin++;
-		return homeWin;
+		$('.homeRuns').text('Wins: '+homeWin);
+		$('.playNext').show();
 	}
+	seriesWinner(awayWin, homeWin);
 }
+function seriesWinner (awayWin,homeWin) {
+	console.log(homeWin + " h a" +awayWin);
+	if (awayWin === 4) {
+		$('.playNext').hide();
+		$('.restart').show();
+		$('.seriesWinner').show();
+		$('.seriesWinner').text(awayTeam + " have beaten the " +homeTeam + " and won the World Series");
+	}
+	else if (homeWin === 4) {
+		$('.playNext').hide();
+		$('.restart').show();
+		$('.seriesWinner').show();
+		$('.seriesWinner').text(homeTeam + " have beaten the " +awayTeam + " and won the World Series");
+	}
 
-
+}
 // });
--- readme.md
+++ readme.md
@@ -1,7 +1,14 @@
-Project 0 - Racing Game
+# Project 0 - Pennant Race
 
-Technologies Used
-
-Existing Features
-
-Planned Features
+# Technologies Used
+ - JacaScript/jQuery
+ - HTML
+ - CSS
+# Existing Features
+ - Select a team and play for the World Series
+ - Countdown timer before beginning play
+ - Reset buttons to start over
+# Planned Features
+ - Add dynamic CSS backgrounds/colors when a team is selected
+ - Adjust the animation for the moving pieces
+ - Improve the CSS styling
